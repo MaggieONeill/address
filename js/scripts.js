9f44ccad85985e562f410440c9f6e92ce0e00af7
teresa fletcher & maggie oneill <student@epicodus.com>
2015-09-21 09:38:53 -0700

    add first objects to sripts and html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-var
-
 $(document).ready(function() {
-  $("form#addressbook").submit(function(event) {
-    var names = ($("input#name").val());
-    $(".complete").text(name);
-
-
-     $("#result").show();
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
-   });
+
+    var inputtedFirstName = $("input#first-name").val();
+    var inputtedLastName = $("input#last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName,
+      address: inputtedAddress };
+
+    $("ul#contacts").append("<li><span class='contact'"> + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-address").val("");
+
+  });
 });
